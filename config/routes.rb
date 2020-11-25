868b24419af2fc6a6e6f44d3da6f22869e84e43a
yuuto.kawasaki <baseball.8796@gmail.com>
2020-11-25 13:18:48 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
   root to: 'tasks#index'
   
   get 'login', to: 'sessions#new'
@@ -15,5 +8,5 @@ Rails.application.routes.draw do
   resources :tasks
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:new, :create]
 end
